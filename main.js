a5708a01a2174cdf4e45174943d078019c7a7aad
ReuvenT <rtrabin@ix.netcom.com>
2025-02-27 08:26:46 -0800

    combined (but needs styling) timeline and tree

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const baseTimelineUrl = 'https://www.tiki-toki.com/timeline/embed/';
+const rootTimeline = "2138285/2648138406/";
+
+
 window.onload = function(){
   // Here is how you would call the libary
   PanZoom(".panzoom");
@@ -6,16 +10,46 @@ localStorage.setItem('selectedOrgItem', null);
 google.charts.load('current', { packages: ["orgchart"] });
 google.charts.setOnLoadCallback(drawChart);
 
-document.addEventListener('DOMContentLoaded', function(){ 
-    console.log("org chart document ready");
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("parent document ready");
     window.addEventListener('message',
-        (event) => {
-            console.log("someone's calling the orgchart listener with event data " + JSON.stringify(event.data));
-            navigateToNode(JSON.parse(event.data).nodeElId);
-        },
-        false
+      (event) => {
+        console.log("someone's calling the parent method with event data " + JSON.stringify(event.data));
+        if (event.data.from === 'org-chart' && event.data.url) {
+          redirectTimelineiFrame(event.data.url)
+        }
+      },
+      false
     );
-}, false);
+
+    const iframe = document.getElementById('tl-timeline-iframe');
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+    iframe.addEventListener('load', () => {
+      console.log('iFrame loaded');
+    });
+
+    iframe.onload = function () {
+      console.log('Iframe content loaded or reloaded');
+    };
+
+
+    setTimeout(() => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const redirectedTimeline = decodeURI(urlParams.get('newtimeline'));
+      //const redirectedTimeline = HttpContext.Current.Request.QueryString("newtimeline")
+      if (redirectedTimeline && redirectedTimeline !="null") {
+        urlParams.delete("redirectedTimeline");
+        console.log('redirectedTimeline: ' + decodeURI(redirectedTimeline));
+        redirectiFrames(baseTimelineUrl + redirectedTimeline, redirectedTimeline);
+      }
+      else{
+        redirectiFrames(baseTimelineUrl + rootTimeline, rootTimeline);
+      }
+    }, 250);
+  }, false);
+
 
 async function drawChart() {
     let fName = "/data/familytreedata.csv";
@@ -40,4 +74,63 @@ async function drawChart() {
     // Draw the chart, setting the allowHtml option to true for the tooltips.
     chart.draw(renderedTable, drawOptions);
 }
+
+if (window.postMessage) {
+    var tlMouseupFunc = function () {
+      var tlFrame = document.getElementById("tl-timeline-iframe");
+      if (tlFrame.contentWindow && tlFrame.contentWindow.postMessage) {
+        tlFrame.contentWindow.postMessage("mouseup", "*");
+      }
+    }
+    if (typeof window.addEventListener != "undefined") {
+      window.addEventListener("mouseup", tlMouseupFunc, false);
+    }
+    else if (typeof window.attachEvent != "undefined") {
+      window.attachEvent("onmouseup", tlMouseupFunc);
+    }
+  }
+
+  function redirectiFrames(timeframeUrl, orgChartElId) {
+    redirectTimelineiFrame(timeframeUrl);
+    redirectOrgchartiFrame(orgChartElId);
+  }
+  function redirectTimelineiFrame(newUrl) {
+    // set timeline target
+    document.getElementById('tl-timeline-iframe').src = newUrl;
+  }
+
+  function redirectOrgchartiFrame(orgChartNodeId) {
+    // set orgchart target
+    // try {
+    //   let ocframe = document.getElementById('orgchart-iframe');
+    //   if (ocframe.contentWindow && ocframe.contentWindow.postMessage) {
+    //     console.log('posting message to orgchart: ' + orgChartNodeId)
+    //     ocframe.contentWindow.postMessage(JSON.stringify({ nodeElId: orgChartNodeId, status: true }, "*"));
+    //   }
+    // } catch (e) {
+    //   window.console && window.console.log(e);
+    // }
+  }
+
+  function toggleTimeline(primaryiFrameId) {
+    let tlFrame = document.getElementById("tl-timeline-iframe");
+    let ocFrame = document.getElementById("orgchart-iframe");
+    if (primaryiFrameId == "tl-timeline-iframe") {
+      if (tlFrame.classList.contains("fullScreen")) {
+        tlFrame.classList.remove("fullScreen");
+        ocFrame.classList.remove("fullScreen");          }
+      else {
+        tlFrame.classList.add("fullScreen");
+      }
+    }
+    else {
+      if (ocFrame.classList.contains("fullScreen")) {
+        tlFrame.classList.remove("fullScreen");
+        ocFrame.classList.remove("fullScreen");
+      }
+      else {
+        ocFrame.classList.add("fullScreen");
+      }
+    }
+  }
 
