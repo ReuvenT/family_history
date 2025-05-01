1643d67300e4dbfc0dbf74df465ec2f6dffe0d65
ReuvenT <rtrabin@ix.netcom.com>
2025-05-01 12:05:52 -0700

    styling chages for menu; timelinebackup.ps1 to source control

--- familytreedata.js
+++ familytreedata.js
@@ -10,12 +10,12 @@ const familyTreeSource = `# timelineId suffixes that link the timeline to the tr
 # Below are the data used to populate the tree struction
 # data columns: Id/Key_Name, parent Id, display content, tooltip, timeline id, panel hash (leave empty for first panel)
 ####### root (Louis)
-LOU_TRA,,Louis "Yuda Lieb" and Freida (Kurtzman) Trabin,tool tip,2138285/2648138406/,,{'menu': 'Trabin Family'; 'access': 'open'; 'seq': 1; 'level': 1}
+LOU_TRA,,Louis "Yuda Lieb" and Freida (Kurtzman) Trabin,tool tip,2138285/2648138406/,,{'menu': 'Trabin Family'; 'access': 'open'; 'seq': 10000; 'level': 1}
 ####### Sam
-SAM_TRA,LOU_TRA,Shrul Sam "Israel" and Celia (Feldman) Trabin,,2141912/5281753800/,,{'menu': 'Sam Trabin Family'; 'access': 'gated'; 'seq': 20; 'level': 2}
+SAM_TRA,LOU_TRA,Shrul Sam "Israel" and Celia (Feldman) Trabin,,2141912/5281753800/,,{'menu': 'Sam Trabin Family'; 'access': 'gated'; 'seq': 20000; 'level': 2}
 NC1_TRA,SAM_TRA,No recorded children,,,
 ####### Ida and descendents
-IDA_TRA,LOU_TRA,Ida (Trabin) and Chaim Kilberg,,2141914/5281753800/,,{'menu': 'Ida (Trabin) Kilberg Family'; 'access': 'gated'; 'seq': 30; 'level': 2}
+IDA_TRA,LOU_TRA,Ida (Trabin) and Chaim Kilberg,,2141914/5281753800/,,{'menu': 'Ida (Trabin) Kilberg Family'; 'access': 'gated'; 'seq': 30000; 'level': 2}
 ALB_KIL,IDA_TRA,Al and Selma Kilberg,,,
 JAN_KIL,ALB_KIL,Jane Kilberg,,,
 TOM_KIL,ALB_KIL,Tom Kilberg,,,
@@ -29,7 +29,7 @@ ELI_OJA,KAR_SCH,Elisa Ojeda,,,
 AMA_OJA,KAR_SCH,Amanda Ojeda,,,
 NAT_KIL,IDA_TRA,Nathan Kilberg,,,
 ####### Harry and descendents
-HAR_TRA,LOU_TRA,Gershon "Harry" and Henrietta (Surman) Trabin ,,2141911/5281753800/,,{'menu': 'Harry Trabin Family'; 'access': 'gated'; 'seq': 30; 'level': 2}
+HAR_TRA,LOU_TRA,Gershon "Harry" and Henrietta (Surman) Trabin ,,2141911/5281753800/,,{'menu': 'Harry Trabin Family'; 'access': 'gated'; 'seq': 30000; 'level': 2}
 ####### Bernie and descendents
 BER_TRA,HAR_TRA,Bernard and Frances (Latzer) Trabin,,,
 TOM_TRA,BER_TRA,Tom Trabin and Nancy Rakela,,,
@@ -53,8 +53,8 @@ VIH_OSH,EVE_OSH,Vihaan Osheroff,,,
 SCO_OSH,FAT_TRA,Scott Osheroff,,,
 AND_TRA,EDW_TRA,Andrew "Andy" Jay Trabin,,,
 ####### Jacob and descendents
-JAC_TRA,LOU_TRA,Jankel "Jacob" and Sadie (Weiss) Trabin,,2139216/5281753800/,,{'menu': 'Jacob Trabin Family'; 'access': 'open'; 'seq': 40; 'level': 2}
-CAR_TRA,JAC_TRA,Carl and Laura (Schacter) Trabin,,2139607/5812857090/,,{'menu': 'Carl Trabin Family'; 'access': 'open'; 'seq': 41; 'level': 3}
+JAC_TRA,LOU_TRA,Jankel "Jacob" and Sadie (Weiss) Trabin,,2139216/5281753800/,,{'menu': 'Jacob Trabin Family'; 'access': 'open'; 'seq': 40000; 'level': 2}
+CAR_TRA,JAC_TRA,Carl and Laura (Schacter) Trabin,,2139607/5812857090/,,{'menu': 'Carl Trabin Family'; 'access': 'open'; 'seq': 41000; 'level': 3}
 SAN_TRA,CAR_TRA,Sandi (Trabin) Advocat<b/><div style='font-style:italic; font-size:small; font-weight: 400;'> m/div: Steve Advocat,,2139607/5812857090/#vars!panel=19750573!
 ROB_ADV,SAN_TRA,Robin (Advocat) Mancol<b/><div style='font-style:italic; font-size:small; font-weight: 400;'> m: Matt Mancol,,,
 HAN_ADV,ROB_ADV,Hannah Advocat,,,
@@ -66,7 +66,7 @@ SHI_BLA,JEN_TRA,Shira Black,,,
 MIL_BLA,JEN_TRA,Talia Black,,,
 ELA_BLA,JEN_TRA,Eliana Black,,,
 DEB_TRA,JAK_TRA,Deb Trabin,,,
-REU_TRA,CAR_TRA,Robert "Reuven" and Bracha (Sabanek) Trabin,,2139607/5812857090/#vars!panel=19740093!
+REU_TRA,CAR_TRA,Robert "Reuven" and Bracha (Sabanek) Trabin,,2167412/5812857090/,,{'menu': 'Reuven Trabin Family'; 'access': 'open'; 'seq': 41300; 'level': 4}
 EIT_TRA,REU_TRA,Eitan Trabin<b/><div style='font-style:italic; font-size:small; font-weight: 400;'> m/div: Natalie Stern,,,
 MIR_TRA,EIT_TRA,Mirko Trabin,,,
 MLN_TRA,EIT_TRA,Milan Trabin,,,
@@ -77,7 +77,7 @@ AYL_DUV,MAY_TRA,Ayla Duval,,,
 DON_TRA,REU_TRA,Donna (Trabin) Avidan<b/><div style='font-style:italic; font-size:small; font-weight: 400;'> m/div: Idan Avidan,,,
 YAN_AVI,DON_TRA,Yanai Avidan,,,
 ELI_AVI,DON_TRA,Eliya Avidan,,,
-SYD_TRA,JAC_TRA,Sydney and Ruth (Zaresky) Trabin,,2165939/0328610175/,,{'menu': 'Sydney Trabin Family'; 'access': 'gated'; 'seq': 41; 'level': 3}
+SYD_TRA,JAC_TRA,Sydney and Ruth (Zaresky) Trabin,,2165939/0328610175/,,{'menu': 'Sydney Trabin Family'; 'access': 'gated'; 'seq': 42000; 'level': 3}
 JAY_TRA,SYD_TRA,Jay and Sherri (Steinberg) Trabin,,,
 DAN_TRA,JAY_TRA,Danielle Trabin,,,
 JOS_TRA,JAY_TRA,Josh Trabin,,,
@@ -85,13 +85,13 @@ ERI_TRA,JAY_TRA,Eric Trabin,,,
 RON_TRA,SYD_TRA,Ron And Janice (Cole) Trabin,,,
 REB_TRA,RON_TRA,Rebecca Trabin,,,
 BEN_TRA,RON_TRA,Benjamin Trabin,,,
-HEL_TRA,JAC_TRA,Helene (Trabin) and Daniel Berne,,2141156/0328610175/,,{'menu': 'Helene (Trabin) Berne Family'; 'access': 'gated'; 'seq': 42; 'level': 3}
+HEL_TRA,JAC_TRA,Helene (Trabin) and Daniel Berne,,2141156/0328610175/,,{'menu': 'Helene (Trabin) Berne Family'; 'access': 'gated'; 'seq': 43000; 'level': 3}
 DOU_BER,HEL_TRA,Doug and Cheryl Berne,,,
 CAN_BER,DOU_BER,Candace Berne,,,
 LIA+BER,DOU_BER,Lianne Berne,,,
 JAM_BER,HEL_TRA,James Berne,,,
 ####### Jenny and descendents
-SHE_TRA,LOU_TRA,Sheindel "Jenny" (Trabin) and Samuel Herlich,,2141915/5281753800/,,{'menu': 'Jenny (Trabin) Herlich Family'; 'access': 'gated'; 'seq': 50; 'level': 2}
+SHE_TRA,LOU_TRA,Sheindel "Jenny" (Trabin) and Samuel Herlich,,2141915/5281753800/,,{'menu': 'Jenny (Trabin) Herlich Family'; 'access': 'gated'; 'seq': 50000; 'level': 2}
 RUT_HER,SHE_TRA,Ruth (Herlich) and Marty Titleman,,,
 MAR_TIT,RUT_HER,Richard Herlich,,,
 CHI_TIT,RUT_HER,?? Herlich,,,
@@ -99,13 +99,13 @@ EST_HER,SHE_TRA,Ester (Herlich) Beifel Lazar,,,
 STE_BEI,EST_HER,Steve Beifel,,,
 BAR_BEI,EST_HER,Barbara Beifel,,,
 ####### Simon and descendents
-SHI_TRA,LOU_TRA,Simon "Shimon" and Esther (Kaufman) Trabin,,2147311/5281753800/,,{'menu': 'Simon Trabin Family'; 'access': 'gated'; 'seq': 60; 'level': 2}
+SHI_TRA,LOU_TRA,Simon "Shimon" and Esther (Kaufman) Trabin,,2147311/5281753800/,,{'menu': 'Simon Trabin Family'; 'access': 'gated'; 'seq': 60000; 'level': 2}
 DEL_PIL,SHI_TRA,Delores (Trabin) and Ronald Pillet,,,
 AND_MAD,DEL_PIL,Andrea (Pillet) and Michael Madden,,,
 STA_PIL,DEL_PIL,Stacey Pillet,,,
 JRM_PIL,SHI_TRA,Jerome "Jack" and Theodora (Rosenblatt) Trabin,,,
 ####### Phillip and descendents
-PHI_TRA,LOU_TRA,Phillip and Mary (Chef?) Trabin,,2141910/5281753800/,,{'menu': 'Philip Trabin Family'; 'access': 'gated'; 'seq': 70; 'level': 2}
+PHI_TRA,LOU_TRA,Phillip and Mary (Chef?) Trabin,,2141910/5281753800/,,{'menu': 'Philip Trabin Family'; 'access': 'gated'; 'seq': 70000; 'level': 2}
 COR_BAC,PHI_TRA,Corinne and Francis Baca,,,
 MIC_BAC,COR_BAC,Michael and Joyce (?) Baca,,,
 CH1_BAC,MIC_BAC,Baca grandchild 1,,,
@@ -118,7 +118,7 @@ LIS_SUC,SEY_TRA,Lisa (Suckiel) and Peter Ockerman,,,
 IAN_OCK,LIS_SUC,Ian Ockerman,,,
 RAC_OCK,LIS_SUC,Rachel Ockerman,,,
 ####### Fanny and descendents
-FAN_TRA,LOU_TRA,Feige "Fanny" (Trabin) and Harry Barnow,,2147312/5281753800/,,{'menu': 'Fanny (Trabin) Barnow Family'; 'access': 'gated'; 'seq': 80; 'level': 2}
+FAN_TRA,LOU_TRA,Feige "Fanny" (Trabin) and Harry Barnow,,2147312/5281753800/,,{'menu': 'Fanny (Trabin) Barnow Family'; 'access': 'gated'; 'seq': 80000; 'level': 2}
 EST_BAR,FAN_TRA,Estelle (Barnow) Katz,,,
 BY1_KAT,EST_BAR,Boy 1,,,
 BY2_KAT,EST_BAR,Boy 2,,,
---
+++ timelinebackup.ps1
@@ -0,0 +1,71 @@
+# from vs code
+# cd C:\Users\rtrab\OneDrive\Development\backups
+# open the timelinebackup.ps1 file, and click the run button
+
+$outputRoot = $PWD.Path + "\" #C:\Users\rtrab\OneDrive\Development\backups\"
+$bkupFolder = $outputRoot + (Get-Date -Format "yyyy-MM") + "\"
+$aggregateContent = ""
+Write-Host "bkupFolder: " + $bkupFolder
+$destinationZipPath = $bkupFolder + "tlbkup" + (Get-Date -Format "yyyy-MM-dd-hh") + ".zip" #Get-Date -Format "yyyy-MM-dd-hh"
+Write-Host "destinationZipPath: " + $destinationZipPath
+
+if (Test-Path -Path $bkupFolder -PathType Container) {}
+else {
+    New-Item -Path $outputRoot -Name (Get-Date -Format "yyyy-MM") -ItemType Directory
+}
+
+# Create the zip archive with this file
+if (Test-Path $destinationZipPath) {
+    Remove-Item $destinationZipPath -verbose
+}
+
+Compress-Archive -Path ".\timelinebackup.ps1" -DestinationPath $destinationZipPath -CompressionLevel Optimal
+#New-Item -Path $destinationZipPath -ItemType file  # new empty zip file
+
+$invalidChars = [regex]::escape([string][char[]][System.IO.Path]::GetInvalidFileNameChars())
+
+$arrTimelines = @(
+    "2138285/Trabin-Family"
+    "2147312/Fanny-Trabin-Barnow-Family"
+    "2141911/Harry-Trabin-Family"
+    "2139216/Jacob-Trabin-Family"
+    "2139607/Carl-Trabin-Family"
+    "2167412/Reuven-Trabin-Family"
+    "2165939/Sydney-Trabin-Family"
+    "2141156/Helene-Trabin-Berne-Family"
+    "2141914/Ida-Trabin-Kilberg-Family"
+    "2141915/Jenny-Trabin-Herlich-Family"
+    "2141910/Phillip-Trabin-Family"
+    "2141912/Samuel-Trabin-Family"
+    "2147311/Simon-Trabin-Family"
+)
+
+# extract all the json
+foreach ($timelineSegment in $arrTimelines) {
+    $url = -join("https://www.tiki-toki.com/timeline/entry/", $timelineSegment, "/json/")
+    Write-Output $url
+    $outputFileName = $timelineSegment -replace "[$invalidChars]", "_"
+    #$outputFileName = -join $outputRoot, $timelineSegment -replace "[$invalidChars]", "_",  ".json"
+
+    $backupFilePath = ($bkupFolder + $outputFileName + ".json")
+    Write-Output ($backupFilePath)
+    $tlBackupJson = (Invoke-webrequest -URI $url).content
+    $tlBackupJson | Out-File -FilePath ($backupFilePath)
+    $aggregateContent += $tlBackupJson 
+
+    $file = Get-Item $backupFilePath
+    Write-Host ("output file: " + $backupFilePath + " size: " + $file.Length)
+
+}
+
+# save all the json in one file for searches, etc.
+$aggregateFilePath = ($bkupFolder + "timelines-aggregate.json")
+$aggregateContent | Out-File -FilePath ($aggregateFilePath)
+
+# add all the json files to the Zip file and remove
+$backupFilePath = ($bkupFolder + "*.json")
+Compress-Archive -Path $backupFilePath -DestinationPath $destinationZipPath -Update
+Remove-Item -Path $backupFilePath
+
+
+
